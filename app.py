8b651f87c72c20b99bf92fad7871560ddf0ca7c7
Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
2024-06-28 13:50:50 +1200

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+from yescite import yescite
+
+app = Flask(__name__)
+
+def process_text(input_bbl, input_bib):
+
+    lines_bbl = input_bbl.splitlines()
+    lines_bib = input_bib.splitlines()
+    
+    new_bib = yescite(app=True, lines_bbl=lines_bbl, lines_bib=lines_bib)
+
+    return "\n".join(new_bib)
+
+@app.route('/', methods=['POST','GET'])
+def index():
+
+    input_bbl = ""
+    input_bib = ""
+    output_text = ""
+
+    if request.method == 'POST':    
+        input_bbl = request.form.get('input_bbl')
+        input_bib = request.form.get('input_bib')
+        output_text = process_text(input_bbl, input_bib)
+    
+    return render_template(
+        'index.html', 
+        input_bbl=input_bbl, 
+        input_bib=input_bib, 
+        output_text=output_text
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
