c23b5c2695bca2ede8cd9236d49e45d10fa970af
Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
2024-06-28 22:07:53 +1200

    add docstrings

--- yescite.py
+++ yescite.py
@@ -4,6 +4,16 @@ def lines_bib_to_keep(
         lines_bbl, 
         lines_bib
 ):
+    """Find the entries in lines_bib mentioned in lines_bbl.
+
+        Parameters:
+            lines_bbl (list) -- The lines of a .bbl file
+            lines_bib (list) -- The lines of a .bib file
+
+        Returns:
+            new_bib (list) -- The sublist of lines_bib containing all the 
+                lines for those entries in .bib that are mentioned in .bbl
+    """
     lines_bbl = [
         line for line in lines_bbl if '\\entry{' in line
     ]
@@ -38,6 +48,23 @@ def yescite(
         lines_bbl=None,
         lines_bib=None,
 ):
+    """Wrapper for lines_bib_to_keep to switch between web app and local use.
+
+        Keyword arguments:
+            use_app -- True if used by the web app and False if the function is 
+                to be run locally for saved .bib and .bbl files (default False)
+            path_bbl (str) -- Path for .bbl (default 'example/example.bbl')
+            path_bib (str) -- Path for .bib (default 'example/example.bib')
+            path_yescite (str) -- Path for output (default 'output/yescite.bib')
+            lines_bbl (list) -- The lines of a .bbl file (default None)
+            lines_bib (list) -- The lines of a .bib file (default None)
+        
+        Returns:
+            If not use_app, nothing is returned by the new_bib returned by
+                lines_bib_to_keep is written to the path path_yescite.
+            If use_app, returns lines_bib_to_keep, which the web app will put
+                in a textarea for the user.
+    """
     if not use_app:
         with open(path_bbl, 'r', encoding='utf-8') as f:
             lines_bbl = f.readlines()
