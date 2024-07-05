d5bda002076f96b4296ed5497b3e96d9878ec0c9
Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
2024-07-06 00:46:08 +1200

    note which entries were kept in the yescite

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
     input_bbl = ''
     input_bib = ''
-    output_text = ''
+    output_yescite = ''
 
     if request.method == 'POST':    
         input_bbl = request.form.get('input_bbl')
@@ -16,13 +16,17 @@ def index():
         lines_bbl = input_bbl.splitlines()
         lines_bib = input_bib.splitlines()
         yc = YesCite(lines_bbl, lines_bib)
-        output_text = '\n'.join(yc.yescite)
+        output_yescite = '\n'.join(yc.yescite)
+        output_aliases_used = yc.aliases_used
+        output_aliases_unused = yc.aliases_unused
     
     return render_template(
         'index.html', 
         input_bbl=input_bbl, 
         input_bib=input_bib, 
-        output_text=output_text,
+        output_yescite=output_yescite,
+        output_aliases_used=output_aliases_used,
+        output_aliases_unused=output_aliases_unused,
     )
 
 if __name__ == '__main__':
