0cd297363f70b80f2ee5d9613eeda9315df67b48
Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
2025-09-16 21:10:51 +0200

    Squashed commit of the following:
    commit dae13d49a8f143823ba6d580ad2892156b8b0d27
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Tue Sep 16 21:10:28 2025 +0200
    
        update descriptions
    
    commit 4327e4aef065072fce973573e0d01bdd62ef2c06
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Tue Sep 16 18:26:54 2025 +0200
    
        add bbl-like validation
    
    commit fea203e6fc851a5da26933786260e94f1ddf74bd
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Sun Sep 14 14:52:23 2025 +0200
    
        add some handy development lines
    
    commit 8e73d92fa913eb405353f3b15b60c465dc290ead
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Sun Sep 14 14:52:08 2025 +0200
    
        add bib-like validation
    
    commit b57b211524de119f6916abac2814795786e5e78c
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Sun Sep 14 13:37:47 2025 +0200
    
        remove unused functions
    
    commit f4400916a32de8d669c06b13f96b54f23ef5b428
    Merge: c6b20bc 4262f2d
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Sun Sep 14 13:14:29 2025 +0200
    
        Merge branch 'main' into input-validation
    
    commit c6b20bcdabc85254b37e429d3b3c38d62f82b519
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Fri Sep 12 12:57:09 2025 +0200
    
        explicit validation functions
    
    commit a4f69dfd5414eb1c9734b498180850e3cf434419
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Fri Sep 12 12:34:57 2025 +0200
    
        cap size of input
    
    commit 35f619b3143cb3df9765deb74907f36e1b12011c
    Author: Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
    Date:   Fri Sep 12 09:55:04 2025 +0200
    
        separate input bibs

--- validation.py
+++ validation.py
@@ -6,13 +6,42 @@ load_dotenv()
 def utf8len(s):
     return len(s.encode('utf-8'))
 
-def valid_yescite(input_bbl, input_bib_YesCite):
+def some_line_starts_with(lines, symbol):
+    return sum([x.lstrip().startswith(symbol) for x in lines]) > 0
+
+# Validation for each input
+
+def valid_bbl(input_bbl):
+    lines_bbl = input_bbl.splitlines()
     return (
         utf8len(input_bbl) < int(os.getenv("INPUT_LIMIT"))
-        and utf8len(input_bib_YesCite) < int(os.getenv("INPUT_LIMIT"))
+        and some_line_starts_with(lines_bbl, "\entry")
     )
 
-def valid_bibtocsv(input_bib_to_csv):
+def valid_bib(input_bib):
+    lines_bib = input_bib.splitlines()
     return (
-        utf8len(input_bib_to_csv) < int(os.getenv("INPUT_LIMIT"))
+        utf8len(input_bib) < int(os.getenv("INPUT_LIMIT"))
+        and some_line_starts_with(lines_bib, "@")
     )
+
+# Validation for each endpoint
+
+def valid_yescite(input_bbl, input_bib):
+    return (
+        valid_bbl(input_bbl)
+        and valid_bib(input_bib)
+    )
+
+def valid_bibtocsv(input_bib):
+    return (
+        valid_bib(input_bib)
+    )
+
+### DEVELOPMENT
+
+# from yescite import paths_to_lines
+# lines_bbl, lines_bib = paths_to_lines(
+#     os.getenv("PATH_EXAMPLE_BBL"),
+#     os.getenv("PATH_EXAMPLE_BIB"),
+# )
