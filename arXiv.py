5ad3af6d0469fde80bb3c4f446e1799793cff5f9
Malcolm Jones <86656442+malcolm-jones@users.noreply.github.com>
2025-10-03 22:17:44 +0200

    match letters with ornaments

--- arXiv.py
+++ arXiv.py
@@ -12,7 +12,7 @@ def query_title(title):
     segments = []
     new_segment = []
     for n in range(len(l)):
-        if len(re.split('[^a-zA-Z-–]', l[n])) == 1:
+        if len(re.split('[^a-zA-Zà-üÀ-Ü-–]', l[n])) == 1:
             new_segment.append(l[n])
         else:
             segments.append(new_segment)
